3c18ad74bc50ae1a79731cfd3f65191000309043
sovannarapov <sovannarapov9@gmail.com>
2024-03-11 14:40:23 +0700

    add password validation

---
+++ api/lib/validation/password.js
@@ -0,0 +1,9 @@
+import { MIN_LENGTH } from '../../helpers/password.js'
+
+export default function validatePassword(password) {
+  const validLength = password.length >= MIN_LENGTH
+  const containLetter = /[a-zA-Z]/g.test(password)
+  const containNumber = /[0-9]/g.test(password)
+
+  return validLength && containLetter && containNumber
+}
