421623b0947acc4481b54c36729787681332167c
sovannarapov <sovannarapov9@gmail.com>
2024-03-11 14:38:31 +0700

    updated user controller

---
+++ api/controller/user.controller.js
@@ -0,0 +1,27 @@
+import UserService from '../service/user.service.js'
+
+export default class UserController {
+  constructor() {
+    this.userService = new UserService();
+  }
+
+  async createUser(user) {
+    return this.userService.createUser(user);
+  }
+
+  async getUsers() {
+    return this.userService.getUsers();
+  }
+
+  async getUserById(userId) {
+    return this.userService.getUserDetail(userId);
+  }
+
+  async updateUser(user) {
+    return this.userService.updateUser(user);
+  }
+
+  async deleteUser(userId) {
+    return this.userService.deleteUser(userId);
+  }
+}
