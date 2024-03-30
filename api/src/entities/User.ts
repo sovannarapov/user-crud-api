fce1e12a4ef942b90fa21d58746fc53d84eefa05
sovannarapov <sovannarapov9@gmail.com>
2024-03-30 09:56:45 +0700

    improve user entity

--- api/src/entities/User.ts
+++ api/src/entities/User.ts
@@ -1,4 +1,4 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, ObjectIdColumn, Unique } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, ObjectIdColumn } from "typeorm";
 import { AbstractEntity } from "./AbstractEntity";
 import { ObjectId } from 'mongodb';
 import { Transform } from 'class-transformer';
@@ -7,28 +7,27 @@ import { IsEmail } from 'class-validator';
 import { hashPassword } from '../lib/core/helpers/password';
 
 @Entity('users')
-@Unique(['email'])
 export class User extends AbstractEntity {
-  @ObjectIdColumn()
-  @Transform((data) => toString(data.value))
-  _id: ObjectId;
+    @ObjectIdColumn()
+    @Transform((data) => toString(data.value))
+    _id: ObjectId;
 
-  @Column()
-  first_name: string;
+    @Column({ nullable: true })
+    first_name: string;
 
-  @Column()
-  last_name: string;
+    @Column({ nullable: true })
+    last_name: string;
 
-  @Column()
-  @IsEmail({}, { message: 'Please use a valid email address.' })
-  email: string;
+    @Column({ unique: true })
+    @IsEmail({}, { message: 'Please use a valid email address.' })
+    email: string;
 
-  @Column()
-  password: string;
+    @Column()
+    password: string;
 
-  @BeforeInsert()
-  @BeforeUpdate()
-  public async hashPassword(): Promise<void> {
-    !isEmpty(this.password) && (this.password = await hashPassword(this.password));
-  }
+    @BeforeInsert()
+    @BeforeUpdate()
+    public async hashPassword(): Promise<void> {
+        !isEmpty(this.password) && (this.password = await hashPassword(this.password));
+    }
 }
