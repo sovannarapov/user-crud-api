47f41aba849cd1be25f7284ca1ad1fd30f5e2838
sovannarapov <sovannarapov9@gmail.com>
2024-06-25 20:20:44 +0700

    improved folder structure

--- client/src/components/AppBar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <nav>
-    <v-app-bar text app>
-      <v-toolbar-title class="text-uppercase grey--text">
-        <span class="font-weight-light">User CRUD <b>Application</b></span>
-      </v-toolbar-title>
-    </v-app-bar>
-  </nav>
-</template>
